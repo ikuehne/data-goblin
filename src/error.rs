e6e73420fa237dfa9195cb71d8a6100fa1611db0
ikuehne <ikuehne617@gmail.com>
2018-04-03 12:22:41 -0700

    Documentation for error.

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,9 @@ pub enum Error {
     NotUtf8,
     /// An IO operation failed.
     Stream(io::Error),
+    /// The lexer failed for the given reason.
     Lexer(String)
 }
 
+/// Custom result type for data-goblin.
 pub type Result<T> = result::Result<T, Error>;
