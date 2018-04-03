70dd1c1280fb3b934914bcc43c3a9e4a6d0d65f5
ikuehne <ikuehne617@gmail.com>
2018-04-03 12:13:29 -0700

    Documentation.

--- src/tok.rs
+++ src/tok.rs
@@ -1,10 +1,15 @@
 #[derive(Debug, PartialEq)]
+
+/// Datalog lexical tokens.
 pub enum Tok {
     Atom(String),
     Comma,
     CloseParen,
+    /// "."
     Dot,
+    /// ":-"
     Means,
+    /// "?"
     Query,
     OpenParen,
     Variable(String)
