61c3300a92526848fd4045d49f4133e80de91566
ikuehne <ikuehne617@gmail.com>
2018-04-17 15:52:12 -0700

    Change demo main to something more like a REPL.

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,39 @@ pub mod error;
 pub mod lexer;
 pub mod parser;
 pub mod tok;
+pub mod storage;
 
 use std::io;
+use std::io::stdout;
 use std::io::Read;
+use std::io::Write;
 
 fn main() {
-
-    ast::AtomicTerm::Atom("hello".to_string());
     let stdin = io::BufReader::new(io::stdin());
     let lexer = lexer::Lexer::new(
         stdin.chars().map(|res| res.map_err(|e| match e {
             io::CharsError::NotUtf8 => error::Error::NotUtf8,
             io::CharsError::Other(e) => error::Error::Stream(e)
         })));
-    let toks: Vec<tok::Tok> = lexer.map(|r|
-                                        r.unwrap_or_else(|err| {
-                                            println!("{}.", err);
-                                            std::process::exit(1)
-                                        }))
-                                   .collect();
-    println!("Tokens read: {:?}", toks);
+    let toks = lexer.map(|r|
+                         r.unwrap_or_else(|err| {
+                             println!("{}.", err);
+                             std::process::exit(1)
+                         }));
 
-    let parser = parser::Parser::new(toks.into_iter().map(Ok));
-    let lines: Vec<ast::Line> = parser.map(|l|
-                                        l.unwrap_or_else(|err| {
-                                            println!("{}.", err);
-                                            std::process::exit(1)
-                                        }))
-                                   .collect();
-    println!("Lines read: {:?}", lines);
+    let parser = parser::Parser::new(toks.map(Ok));
+    let lines = parser.map(|l| l.unwrap_or_else(|err| {
+        println!("{}.", err);
+        std::process::exit(1)
+    }));
 
+    let prompt = "data-goblin> ";
+    print!("{}", prompt);
+    stdout().flush().unwrap();
+    for line in lines {
+        println!("Read line: {:?}", line);
+        print!("{}", prompt);
+        stdout().flush().unwrap();
+    }
+    println!("Morituri te salutamus.");
 }
