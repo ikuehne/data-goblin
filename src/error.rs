a6638d814f1b312ca6f52f08f6177040de416622
ikuehne <ikuehne617@gmail.com>
2018-04-18 19:58:10 -0700

    Get rid of unused error types.

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,10 @@
 use std::error;
 use std::fmt;
-use std::io;
 use std::result;
 
 /// Errors data-goblin can encounter.
 #[derive(Debug)]
 pub enum Error {
-    NotUtf8,
-    /// An IO operation failed.
-    Stream(io::Error),
     /// The lexer failed for the given reason.
     Lexer(String),
     /// The parser failed for the given reason.
@@ -21,8 +17,6 @@ pub type Result<T> = result::Result<T, Error>;
 impl error::Error for Error {
     fn description(&self) -> &str {
         match self {
-            Error::NotUtf8 => "input not valid UTF-8",
-            Error::Stream(_) => "stream read failed",
             Error::Lexer(_) => "lexer error",
             Error::Parser(_) => "parser error"
         }
@@ -30,8 +24,6 @@ impl error::Error for Error {
 
     fn cause(&self) -> Option<&error::Error> {
         match self {
-            Error::NotUtf8 => None,
-            Error::Stream(e) => Some(e),
             Error::Lexer(_) => None,
             Error::Parser(_) => None
         }
@@ -41,8 +33,6 @@ impl error::Error for Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::NotUtf8 => write!(f, "input not valid UTF-8"),
-            Error::Stream(e) => write!(f, "stream read failed: {}", e),
             Error::Lexer(s) => write!(f, "lexer error: {}", s),
             Error::Parser(s) => write!(f, "parser error: {}", s)
         }
