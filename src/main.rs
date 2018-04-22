055a462966b5ab6fe1bf80a8b6195ab82e5f9551
ikuehne <ikuehne617@gmail.com>
2018-04-21 17:06:38 -0700

    Add durability.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(io)]
 #![feature(option_filter)]
+#![feature(type_ascription)]
 
 pub mod ast;
 pub mod error;
@@ -9,6 +10,11 @@ pub mod parser;
 pub mod tok;
 pub mod storage;
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
 use error::*;
 
 use std::io;
@@ -16,6 +22,8 @@ use std::io::stdout;
 use std::io::Read;
 use std::io::Write;
 use std::fmt::Display;
+
+const DEFAULT_DATA_DIR: &'static str = "./data/";
 
 fn abort<T: Display>(e: T) -> ! {
     eprintln!("Error: {}", e);
@@ -46,7 +54,8 @@ fn main() {
     let prompt = "data-goblin> ";
     print!("{}", prompt);
     stdout().flush().unwrap();
-    let engine = storage::StorageEngine::new();
+    let engine = storage::StorageEngine::new(DEFAULT_DATA_DIR.to_string())
+        .unwrap_or_else(|e| abort(e));
     let mut evaluator = eval::Evaluator::new(engine);
     for line in lines {
         handle_line(&mut evaluator, line).unwrap_or_else(|e| {
--- src/storage.rs
+++ src/storage.rs
@@ -1,9 +1,17 @@
 /// Simple in-memory storage engine for Datalog.
 
 use ast;
+use error::*;
+use error::Error::StorageError;
 
+use serde_json;
+
+use std;
 use std::collections::HashMap;
+use std::fs;
+use std::io;
 use std::iter::IntoIterator;
+use std::path::Path;
 use std::slice;
 
 // Perhaps we want this to be generic in the future to allow swapping out
@@ -13,16 +21,18 @@ use std::slice;
 
 pub type Tuple = Vec<String>;
 
-#[derive(Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Table {
     rows: Vec<Tuple>
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct View {
     formals: Vec<String>,
     definition: Vec<ast::Term>
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum Relation {
     Extension(Table),
     Intension(View)
@@ -52,13 +62,48 @@ impl<'i> IntoIterator for &'i Table {
 }
 
 pub struct StorageEngine {
+    data_dir: String,
     tables: HashMap<String, Relation>
 }
 
+fn err<E: std::error::Error + 'static>(err: E) -> Error {
+    StorageError(Box::new(err))
+}
+
 impl StorageEngine {
-    pub fn new() -> Self {
-        StorageEngine {
-            tables: HashMap::new()
+    pub fn new(data_dir: String) -> Result<Self> {
+        let mut tables = HashMap::new();
+
+        match fs::read_dir(data_dir.clone()) {
+            Err(e) =>
+                match e.kind() {
+                    io::ErrorKind::NotFound => {
+                        fs::create_dir(data_dir.as_str());
+                        Ok(StorageEngine {
+                            data_dir,
+                            tables
+                        })
+                    },
+                    other => Err(err(e))
+                },
+            Ok(files)  => {
+                for res_entry in files {
+                    let entry = res_entry.map_err(err)?;
+                    let fname = entry.path();
+                    let reader = fs::File::open(fname).map_err(err)?;
+                    let buffered = io::BufReader::new(reader);
+                    let table: Relation =
+                        serde_json::from_reader(buffered).map_err(err)?;
+                    let name = entry.file_name().into_string().map_err(|e|
+                        Error::BadFilename(e)
+                    )?;
+                    tables.insert(name, table);
+                }
+                Ok(StorageEngine {
+                    data_dir,
+                    tables
+                })
+            }
         }
     }
 
@@ -72,5 +117,16 @@ impl StorageEngine {
 
     pub fn get_or_create_table(&mut self, name: String) -> &mut Relation {
         self.tables.entry(name).or_insert(Relation::Extension(Table::new()))
+    }
+}
+
+impl Drop for StorageEngine {
+    fn drop(&mut self) {
+        for (fname, table) in &self.tables {
+            let path_buf = Path::new(self.data_dir.as_str()).join(fname);
+            let path = path_buf.as_path();
+            let out = io::BufWriter::new(fs::File::create(path).unwrap());
+            serde_json::to_writer(out, table).unwrap();
+        }
     }
 }
